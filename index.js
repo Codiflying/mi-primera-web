6ecc1b1d8febeb260f80fa661185c6f61b8983fe
Codiflying <96573273+Codiflying@users.noreply.github.com>
2026-03-14 11:58:57 -0500

    Crear landing page de medicina estética y tienda dermatológica

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Bienvenido Codiflying")
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", (event) => {
+    const section = document.querySelector(anchor.getAttribute("href"));
+
+    if (section) {
+      event.preventDefault();
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  });
+});
